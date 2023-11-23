f50ac6b5658bdc9676dce9a6c979f7f8ed8390e7
Valentino Borgo <valentino-2000-dm@hotmail.com>
2023-11-23 11:46:52 -0300

    Primer POST registro

--- src/Controller/RegistroManager.php
+++ src/Controller/RegistroManager.php
@@ -1,4 +1,37 @@
 <?php
 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistroManager extends AbstractController{
+
+    /**
+    * @Route("/registroController", name="mostrar_datos")
+    */
+    public function registrarCliente(Request $request): Response {
+
+        if($request->getMethod() == "POST"){
+            $nombre = $request->request->get('nombre_completo');
+            $nombreUsuario = $request->request->get('nombre_usuario');
+            $correoElectronico = $request->request->get('correo_electronico');
+            $contraseña = $request->request->get('contraseña');
+            $repetirContraseña = $request->request->get('repetir_contraseña');
+            print_r($nombreUsuario);
+            $this->addFlash('exito','Se a registrado correctamente ' . $nombre);
+            return $this->render('registro/registro.html.twig');
+        }
+
+    }
+
+    private function verificarDatosRepetidos(){
+
+    }
+
+}
+
 
 ?>
