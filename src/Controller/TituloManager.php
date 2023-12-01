7784b54ea839a6c0e7d7d07de88bb589ef6cc208
Valentino Borgo <valentino-2000-dm@hotmail.com>
2023-12-01 11:44:17 -0300

    Integration of API YOUTUBE

--- src/Controller/TituloManager.php
+++ src/Controller/TituloManager.php
@@ -9,34 +9,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\Suscripcion;
-use App\Entity\TipoSuscripcion;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Entity\Titulo;
-use App\Entity\Cliente;
-use App\Repository\SuscripcionRepository;
-use App\Repository\TipoSuscripcionRepository;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class TituloManager extends AbstractController{
 
+    private $APIKEY = 'AIzaSyC_kXT-aZcdRZI4wmwlkni9um8eTNlo7G4';
+    private $maxResults = 10;
 
     /**
     * @Route("/titulo/{id}", name="info_titulo")
     */
-    public function mostrarInfoTitulo(ManagerRegistry $doctrine, int $id): Response{
-
-        $usuario = $this->getUser();
-        $nombreUsuario = $usuario->getNombre();
+    public function mostrarInfoTitulo(ManagerRegistry $doctrine, int $id, HttpClientInterface $httpClient): Response{
 
         $repository = $doctrine->getRepository(Titulo::class);
         $titulo = $repository->find($id);
-
+        
         if(!$titulo){
             throw new Exception("Este Titulo no existe");
         }
 
-        dump($titulo);
+        $nombreBusqueda = $titulo->getTitulo();
+
+        if($titulo->getVideo() == ''){
+            $apiURLVIDEO = 'https://www.googleapis.com/youtube/v3/search?key=' . $this->APIKEY . '&type=video&part=snippet&maxResults=' . $this->maxResults . '&q=' .$nombreBusqueda;
+            $responseVIDEO = $httpClient->request('GET', $apiURLVIDEO);
+            $dataVIDEO = $responseVIDEO->toArray();
+            $titulo->setVideo('http://www.youtube.com/embed/' .$dataVIDEO['items'][0]['id']['videoId']);
+            
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($titulo);
+            $entityManager->flush();
+        }
+        
+        
+        $usuario = $this->getUser();
+        $nombreUsuario = $usuario->getNombre();
 
         return $this->render('titulo/info_titulo.html.twig',[
             'nombreUsuario' => $nombreUsuario,
